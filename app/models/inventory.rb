aa50ac4fca4d5de965b18dabdc11787f76677987
29td <nonhlanhlamndebele9@gmail.com>
2022-11-24 08:30:51 +0200

    Create inventory list

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,2 +1,25 @@
 class Inventory < ApplicationRecord
-end
+    belongs_to :user, foreign_key: :user_id, class_name: 'User'
+    has_many :inventory_foods, foreign_key: :inventory_id, dependent: :destroy
+    has_many :foods, through: :inventory_foods
+    
+    validates :name, presence: true
+    validates :description, presence: true
+  
+    def self.recent_inventries(id)
+      inventory = Inventory.find(id)
+      inventory_foods = inventory.recipeFoods
+      @total = inventory_foods.map { |x| InventoryFood.value(x.id) }.reduce(:+)
+      @total = if @total.nil?
+                 0
+               else
+                 @total
+               end
+    end
+  
+    def self.items(id)
+      inventory = Inventory.find(id)
+      inventory.inventoryFoods.count
+    end
+  end
+  
--- app/models/inventory_food.rb
+++ app/models/inventory_food.rb
@@ -1,2 +1,11 @@
 class InventoryFood < ApplicationRecord
-end
+    belongs_to :inventory, foreign_key: :inventory_id, class_name: 'Inventory'
+    belongs_to :food, foreign_key: :food_id, class_name: 'Food'
+  
+    def self.value(id)
+      inventory_food = InventoryFood.find(id)
+      food = inventory_food.food
+      inventory_food.quantity
+    end
+  end
+  
