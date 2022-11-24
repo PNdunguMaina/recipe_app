555a8e5ce45210a61bed72bfe9a04823eb96d314
29td <nonhlanhlamndebele9@gmail.com>
2022-11-24 08:39:20 +0200

    Create inventory list

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :foods
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  resources :foods, only: [:index, :new, :create, :destroy]
-  resources :recipes, only: [:index, :new, :create, :destroy, :show] do
-    resources :recipe_foods, only: [:new, :edit, :create, :destroy, :update]
-  end
-  resources :public, only: [:index]
-  resources :general, only: [:index]
-
 
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_for :users
+  root :to => "inventoryfoods#index"
+  
+   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+   resources :foods, only: [:index, :new, :create, :destroy]
+   resources :recipes, only: [:index, :new, :create, :destroy, :show] do
+     resources :recipe_foods, only: [:new, :edit, :create, :destroy, :update]
+   end
+
+   resources :inventory, only: [:index, :new, :create, :destroy, :show] do
+    resources :inventory_foods, only: [:new, :edit, :create, :destroy, :update]
+  end
+
+   resources :public, only: [:index]
+   resources :general, only: [:index]
+
 end
