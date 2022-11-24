bc1490ec6163e99857d562d958154adfbf16c7b9
29td <nonhlanhlamndebele9@gmail.com>
2022-11-24 08:50:45 +0200

    Create inventory list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,19 @@
 class ApplicationController < ActionController::Base
-    before_action :update_allowed_parameters, if: :devise_controller?
-    before_action :authenticate_user!
-  
-    protected
-  
-    def update_allowed_parameters
-      devise_parameter_sanitizer.permit(:sign_up) do |u|
-        u.permit(:name, :email, :password, :password_confirmation, :email_confirmation)
-      end
-      devise_parameter_sanitizer.permit(:account_update) do |u|
-        u.permit(:name, :email, :password, :current_password)
-      end
+  before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :email, :password, :password_confirmation, :email_confirmation)
     end
-  
-    def after_sign_in_path_for(_resource)
-      root_path
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :password, :current_password)
     end
   end
-  
+
+  def after_sign_in_path_for(_resource)
+    root_path
+  end
+end
--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,4 @@
 class FoodsController < ApplicationController
-
-  
   def index
     @user = current_user
     @foods = @user.foods.all
