8a04e581f21476d161bfbf3028826cd743c0b49a
PNdunguMaina <ndunguwamaina@gmail.com>
2022-11-26 14:11:58 +0300

    Add public recipes list to the dashboard

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,17 @@ class Recipe < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true
+
+  def count_total_price
+    total = 0
+    recipe_foods.each do |rf|
+      food = Food.find(rf.food_id)
+      total += food.price
+    end
+    total
+  end
+
+  def count_total_food_items
+    recipe_foods.sum('quantity')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :homes
   resources :foods
+  get '/public_recipes', to: 'recipes#public'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "homes#index"
+  root "recipes#public"
 end
